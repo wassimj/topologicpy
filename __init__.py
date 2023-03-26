f5c1a193ecf69ddcfb41a5b2c81f52021a4c1a22
Wassim Jabi <jabiw@cardiff.ac.uk>
2023-03-26 07:10:04 +0100

    Updated to version 0.3.3

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ import sys
 import os, re
 from sys import platform
 
-__version__ = '0.3.2'
+__version__ = '0.3.3'
 __version_info__ = tuple([ int(num) for num in __version__.split('.')])
 
 if platform == 'win32':
