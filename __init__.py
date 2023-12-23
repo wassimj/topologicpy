3a548dd3f5bc32b1c3cc13cb68e0e352d7bdc4e2
Wassim Jabi <jabiw@cardiff.ac.uk>
2023-12-23 09:20:27 +0000

    Updated version number to 0.4.55

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ import sys
 import os, re
 from sys import platform
 
-__version__ = '0.4.54'
+__version__ = '0.4.55'
 __version_info__ = tuple([ int(num) for num in __version__.split('.')])
 
 if platform == 'win32':
