0e4ca8c8bdf268c1126ac771c61e076c541d9e57
Wassim Jabi <jabiw@cardiff.ac.uk>
2024-02-04 08:32:15 +0000

    Updated all files to include AGPL license and disclaimer
    Updated all files to include AGPL license and disclaimer. Also added an AGPL v3 license.txt file

--- Plotly.py
+++ Plotly.py
@@ -1,18 +1,28 @@
+# Copyright (C) 2024
+# Wassim Jabi <wassim.jabi@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along with
+# this program. If not, see <https://www.gnu.org/licenses/>.
+
 import topologicpy
 import topologic
 
-from topologicpy.Vertex import Vertex
-from topologicpy.Edge import Edge
 from topologicpy.Wire import Wire
 from topologicpy.Face import Face
-from topologicpy.Cell import Cell
-from topologicpy.CellComplex import CellComplex
-from topologicpy.Cluster import Cluster
-from topologicpy.Topology import Topology
 import os
 
 try:
-    import numpy
+    import numpy as np
 except:
     print("Plotly - Installing required numpy library.")
     try:
@@ -20,7 +30,7 @@ except:
     except:
         os.system("pip install numpy --user")
     try:
-        import numpy
+        import numpy as np
     except:
         raise Exception("Plotly - Error: Could not import numpy.")
 
