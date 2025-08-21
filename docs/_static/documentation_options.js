032095bd1d29c0ca8577ce2191f4f5fe5e97ed7d
wassimj <wassimj@users.noreply.github.com>
2025-08-21 13:06:35 +0000

    deploy: f09e0e54d4462b1ac7569d2db94e1a68d8d0d853

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.8.50',
+    VERSION: '0.8.51',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
