1071a5302311db027b44956a43a62cc7bfba88f9
Wassim Jabi <jabiw@cardiff.ac.uk>
2026-06-19 19:22:32 +0300

    Introducing TGraph!

--- tests/test_11Grid.py
+++ tests/test_11Grid.py
@@ -52,12 +52,34 @@ def test_main():
     face5 = Face.ByVertices([v1, v5, v4, v0, v1])     # create face
     f_list = [face0, face1, face2, face3, face4, face5]  # create list
 
+    def test_main():
+    print("Start")
+    print("3 Cases")
+    # Object for test case
+    v0 = Vertex.ByCoordinates(0, 0, 0)          # create vertex
+    v1 = Vertex.ByCoordinates(0, 10, 0)         # create vertex
+    v2 = Vertex.ByCoordinates(10, 10, 0)        # create vertex
+    v3 = Vertex.ByCoordinates(10, 0, 0)         # create vertex
+    v4 = Vertex.ByCoordinates(0, 0, 10)         # create vertex
+    v5 = Vertex.ByCoordinates(0, 10, 10)        # create vertex
+    v6 = Vertex.ByCoordinates(10, 10, 10)       # create vertex
+    v7 = Vertex.ByCoordinates(10, 0, 10)        # create vertex
+    list_v = [v0, v1, v2, v3, v4, v5, v6, v7]
+    e0 = Edge.ByStartVertexEndVertex(v0, v1)
+    wire0 = Wire.ByVertices([v0, v1, v2])         # create wire
+    wire1 = Wire.ByVertices([v4, v5, v6])         # create wire
+    w_list = [wire0,wire1]                      # create list
+    face0 = Face.ByVertices([v0, v1, v2, v3, v0])     # create face
+    face1 = Face.ByVertices([v4, v5, v6, v7, v4])     # create face
+    face2 = Face.ByVertices([v0, v4, v7, v3, v0])     # create face
+    face3 = Face.ByVertices([v3, v7, v6, v2, v3])     # create face
+    face4 = Face.ByVertices([v2, v6, v5, v1, v2])     # create face
+    face5 = Face.ByVertices([v1, v5, v4, v0, v1])     # create face
+    f_list = [face0, face1, face2, face3, face4, face5]  # create list
+
     # Case 1 - EdgesByDistances
     print("Case 1")
     # test 1
-    clus_ed = Grid.EdgesByDistances()
-    assert Topology.IsInstance(clus_ed, "Cluster"), "Grid.ByDistances. topologic.Cluster"
-    # test 2
     clus_ed1 = Grid.EdgesByDistances(face=face0, uOrigin=v0, vOrigin=v1, uRange=[-0.5, -0.25, 0, 0.25, 0.5],
                                 vRange=[-0.5, -0.25, 0, 0.25, 0.5], clip=True, tolerance=0.001)
     assert Topology.IsInstance(clus_ed1, "Cluster"), "Grid.ByDistances. topologic.Cluster"
